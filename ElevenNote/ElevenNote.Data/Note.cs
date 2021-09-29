9c320ad02c40a7192404d1edceee6c1f6fd246d2
Otto Lutz <ottolutz00@gmail.com>
2021-09-29 18:14:04 -0400

    Added Note data model

---
+++ ElevenNote/ElevenNote.Data/Note.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data
+{
+    public class Note
+    {
+        [Key]
+        public int NoteId { get; set; }
+        [Required]
+        public Guid OwnerId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        [Display(Name = "Date Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+        [Display(Name = "Date Modified")]
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
